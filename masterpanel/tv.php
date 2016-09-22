3a6cad37b8c4e25164459d051bfc98e04257727e
SwG Ghosh <codecrafts.cf@icloud.com>
2016-09-22 23:24:02 +0530

    Modified style.css, tv.css, tv.php

--- masterpanel/tv.php
+++ masterpanel/tv.php
@@ -26,7 +26,7 @@
         </div>
         <footer>
             <div class="foot">
-                <br>&copy; Infinnovation'16.<br>Made with <span class="heart">&hearts;</span> and JavaScript.<br>
+                <br>&copy; Infinnovation'16.<br>
                 Designed and Developed by <a href="https://facebook.com/swghosh" target="_blank" onclick="window.open('https://www.github.com/swghosh');">Swarup Ghosh</a>.<br>
                 <a href="https://twitter.com/SwG_Ghosh" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @SwG_Ghosh</a>
                 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script><br><br>
@@ -39,6 +39,7 @@
         function refresh() {
             loadStocks("../stocktable.php");
         }
+        refresh();
         setInterval(refresh, 10000);
     </script>
 </html>
