6a18300e428483ea8c229695c572913c880aab45
SwG Ghosh <codecrafts.cf@icloud.com>
2017-07-18 14:39:16 +0530

    Errors in heading of stockadder, stockremover fixed

--- masterpanel/stockadder/index.php
+++ masterpanel/stockadder/index.php
@@ -9,7 +9,7 @@
     </head>
     <body>
         <img src="../../images/logo.png">
-        <h1><a href="../">Infinnovation Stock Exchange Simulator Master Panel</a><br><small>Update Stock Price Section</small></h1>
+        <h1><a href="../">Infinnovation Stock Exchange Simulator Master Panel</a><br><small>Add New Stock Section</small></h1>
         <p><sup>*</sup> Do not use this panel without the permission/authorisation by Infinnovation Stock Exchange Board.</p>
         <br>
         Add operation:<br>
